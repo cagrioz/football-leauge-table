382090f91ac29ea988b4373213f0bfe57cc54da8
Cagri Oz <cagriozarpaciii@hotmail.com>
2020-06-15 15:06:29 +0300

    Finished!
    - DisplayMatchRecords() created
    - Comments leaved
    - Made more dynamic with contants

--- game.c
+++ game.c
@@ -1,8 +1,14 @@
+// Doğu Çağrı Özarpacı - 2471589
+
 /// Libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+// Constants
+#define WIN_POINTS 3
+#define DRAW_POINTS 1
 
 // Structures
 struct Match {
@@ -34,6 +40,7 @@ void OrderLeagueTable(TEAM *, int);
 void DisplayLeagueTable(TEAM *, int);
 int SearchTeam(TEAM *, int, char *);
 void capitalize(char *);
+void DisplayMatchRecords(FILE *);
 
 int main() {
     FILE * matchRecords;
@@ -41,8 +48,10 @@ int main() {
     TEAM * leagueTable;
     char willSearch[80];
 
-    char tableFileName[] = "scoretable.txt";
+    // Name of file that going to be used
+    char tableFileName[] = "CNG140-P5-Dogu-Cagri-Ozarpaci.txt";
 
+    // Create the file stream
     matchRecords = fopen(tableFileName, "r");
 
     // Check if score table file is exist
@@ -50,6 +59,9 @@ int main() {
         printf("Score table file does NOT exist!\n");
         exit(EXIT_FAILURE);
     }
+
+    printf("Score records file has been successully loaded!\nFollowing records have been loaded:\n\n");
+    DisplayMatchRecords(matchRecords);
 
     // Get the number of matches played
     int numOfLines = getNumberOfLines(matchRecords);
@@ -59,7 +71,7 @@ int main() {
 
     // Check if memory is allocated
     if (!scoreTable) {
-        printf("Something went wrong while trying to allocate memory\n");
+        printf("Something went wrong while trying to allocate memory!\n");
         exit(EXIT_FAILURE);
     }
 
@@ -69,34 +81,85 @@ int main() {
     // Create the league table
     leagueTable = CreateLeagueTable(scoreTable, numOfLines);
 
+    // Sort the league table
     OrderLeagueTable(leagueTable, numOfLines);
     
+    // Display the league table
     DisplayLeagueTable(leagueTable, numOfLines);
 
-    gets(willSearch);
+    // Clear the buffer
+    fflush(stdin);
 
-    // Search the team that entered, if it's exist then return the index
-    int indexOf = SearchTeam(leagueTable, numOfLines, willSearch);
-    int rank = indexOf + 1;
+    char wantSearch = 'S';
+    int indexOf, rank;
+    while (wantSearch == 'S' || wantSearch == 's') {
+        printf("Press E for exit or S for search: ");
+        scanf("\n%c", &wantSearch);
+        fflush(stdin);
 
-    capitalize(willSearch);
+        if (wantSearch == 'S' || wantSearch == 's') {
+            printf("Enter the name of the team: ");
 
-    if (indexOf != -1) {
-        printf("%s’s rank is %d!", willSearch, rank);
-    } else {
-        printf("That team is unknown! Please try again!");
+            // Get a team name from user that going to be searched in league table
+            scanf("\n%s", willSearch);
+            fflush(stdin);
+
+            // Search the team that entered, if it's exist then return the index
+            indexOf = SearchTeam(leagueTable, numOfLines, willSearch);
+            rank = indexOf + 1;
+
+            // Capitalize the user's input to display
+            capitalize(willSearch);
+
+            // Check if the team user entered is exist in the league table
+            if (indexOf != -1) {
+                printf("%s's rank is %d!\n", willSearch, rank);
+            } else {
+                printf("That team is unknown! Please try again!\n");
+            }
+
+        } else  {
+            break;
+        }
     }
+
+    printf("Wishing you a successful week!\n");
 
     // Close the files that openeed
     fclose(matchRecords);
 
-    // TODO: Set pointers free
+    // Set pointers free
     free(scoreTable);
     free(leagueTable);
 
     return 0;
 }
 
+void DisplayMatchRecords(FILE * matchRecords) {
+    printf("Formed Leauge Table is as follows:\n\n");
+    printf("  Team1        Team2            Team1Score   Team2Score\n");
+    printf("  -----------------------------------------------------\n");
+    
+    char firstTeam[80], secondTeam[80];
+    int firstScore, secondScore;
+    while (!feof(matchRecords)) {
+        fscanf(matchRecords, "%s %s %d %d", firstTeam, secondTeam, &firstScore, &secondScore);
+        printf("  %-12s %-12s %5d %12d\n", firstTeam, secondTeam, firstScore, secondScore);
+    }
+
+    printf("\n\n");
+    rewind(matchRecords);
+}
+
+/**
+ * This function counts the number of new lines (rows) in a file '\'
+ * 
+ * @param FILE fileStream
+ *   File that number of new lines will be counted
+ * 
+ * @return int
+ *   return number of rows
+*/
 int getNumberOfLines(FILE * fileStream) {
     char el;
     int numOfLines = 1;
@@ -115,6 +178,14 @@ int getNumberOfLines(FILE * fileStream) {
     return numOfLines;
 }
 
+/**
+ * This function reads the the match datas in a file
+ * 
+ * @param FILE matchRecords
+ *   File that has match datas
+ * @param MATCH scoreTable
+ *   Array of structure that will have our match datas
+*/
 void LoadScoreTable(FILE * matchRecords, MATCH * scoreTable) {
     int i = 0;
     while (!feof(matchRecords)) {
@@ -123,6 +194,16 @@ void LoadScoreTable(FILE * matchRecords, MATCH * scoreTable) {
     }
 }
 
+/**
+ * This function creates a league table according to match records that fetched from a file
+ * 
+ * @param FILE matchRecords
+ *   File that has match datas
+ * @param MATCH scoreTable
+ *   Array of structure that will have our match datas
+ * 
+ * @return TEAM * {teamName, win, draw, loss, avg, points, goals} a structure for each team
+*/
 TEAM * CreateLeagueTable(MATCH * scoreTable, int size) {
     TEAM * leagueTable;
     int teamCount = size * 2;
@@ -132,7 +213,7 @@ TEAM * CreateLeagueTable(MATCH * scoreTable, int size) {
 
     // Check if memory is allocated
     if (!leagueTable) {
-        printf("Something went wrong while trying to allocate memory\n");
+        printf("Something went wrong while trying to load the league table!\n");
         exit(EXIT_FAILURE);
     }
 
@@ -146,7 +227,6 @@ TEAM * CreateLeagueTable(MATCH * scoreTable, int size) {
         leagueTable[i].goals = 0;
     }
 
-    //TEAM tempFirst, tempSecond;
     for (int i = 0; i < size; i++) {
         // Put each team to the leage table
         strcpy(leagueTable[i].teamName, scoreTable[i].firstTeam);
@@ -169,8 +249,8 @@ TEAM * CreateLeagueTable(MATCH * scoreTable, int size) {
         }
 
         // Calculation of the points
-        leagueTable[i].points  = (leagueTable[i].win * 3) + (leagueTable[i].draw * 1);
-        leagueTable[i + size].points  = (leagueTable[i + size].win * 3) + (leagueTable[i + size].draw * 1);
+        leagueTable[i].points  = (leagueTable[i].win * WIN_POINTS) + (leagueTable[i].draw * DRAW_POINTS);
+        leagueTable[i + size].points  = (leagueTable[i + size].win * WIN_POINTS) + (leagueTable[i + size].draw * DRAW_POINTS);
 
         // Calculate the average
         leagueTable[i].average  = scoreTable[i].firstScore - scoreTable[i].secondScore;
@@ -180,6 +260,14 @@ TEAM * CreateLeagueTable(MATCH * scoreTable, int size) {
     return leagueTable;
 }
 
+/**
+ * This function sorts the teams based on their stats
+ * 
+ * @param TEAM * leagueTable
+ *   Arroy of structure that holds the teams' stats
+ * @param MATCH size
+ *   Number of lines or matches played between 2 teams (multiply 2 to get total number of teams)
+*/
 void OrderLeagueTable(TEAM * leagueTable, int size) {
     size *= 2;
 
@@ -217,16 +305,38 @@ void OrderLeagueTable(TEAM * leagueTable, int size) {
     }
 }
 
+/**
+ * Display league table which contains stats of each team
+ * 
+ * @param TEAM * leagueTable
+ *   Arroy of structure that holds the teams' stats
+ * @param MATCH size
+ *   Number of lines or matches played between 2 teams (multiply 2 to get total number of teams)
+*/
 void DisplayLeagueTable(TEAM * leagueTable, int size) {
     size *= 2;
 
-    printf("  Teams    d        Win    Draw    Loss    Points    Average    Goals\n");
+    printf("Formed Leauge Table is as follows:\n\n");
+    printf("  Teams            Win  Draw    Loss    Points    Average    Goals\n");
     printf("  ----------------------------------------------------------------------\n");
+
     for (int i = 0; i < size; i++) {
         printf("  %-14s %3d %4d %7d %7d %9d %10d\n", leagueTable[i].teamName, leagueTable[i].win, leagueTable[i].draw, leagueTable[i].loss, leagueTable[i].points, leagueTable[i].average, leagueTable[i].goals);
     }
+
+    printf("\n");
 }
 
+/**
+ * Search for the team that is entered by the user
+ * 
+ * @param TEAM * leagueTable
+ *   Arroy of structure that holds the teams' stats
+ * @param MATCH size
+ *   Number of lines or matches played between 2 teams (multiply 2 to get total number of teams)
+ * @param char * willSearch
+ *   A string obtained from user 
+*/
 int SearchTeam(TEAM * leagueTable, int size, char * willSearch) {
     size *= 2;
 
@@ -239,6 +349,12 @@ int SearchTeam(TEAM * leagueTable, int size, char * willSearch) {
     return -1;
 }
 
-void capitalize(char *s) {
-    *s = toupper(*s); 
+/**
+ * Capitalizes a string
+ * 
+ * @param char * str
+ *   String that going to be capitalized
+*/
+void capitalize(char *str) {
+    *str = toupper(*str); 
 }
